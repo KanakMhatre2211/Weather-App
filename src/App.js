e9ef80578fca39a67eac54b0ef77bf1dbc282519
Kanak Mhatre <Kanak@rasheedfoundation.org>
2024-05-15 00:53:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Weather from './weather';
+import Forecast from './forecast';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather/>
+      <section className='app'>
+        <Forecast />
+      </section>
+    </div>
+  );
+}
+
+export default App;
